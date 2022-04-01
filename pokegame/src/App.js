260b07c40502c73e88cf74b073647d54a79a6210
Raghav <raghav_sobti@yahoo.com>
2022-04-01 14:45:25 +0530

    Finalised Project
    Finalised Project

--- pokegame/src/App.js
+++ pokegame/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Pokedex from "./Pokedex";
+import Pokegame from "./Pokegame";
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <Pokegame />
     </div>
   );
 }
